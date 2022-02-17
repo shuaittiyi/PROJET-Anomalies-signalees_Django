5d5a567837f58a22cd0da53e156c8ec3b806451d
shuaittiyi <95615474+shuaittiyi@users.noreply.github.com>
2022-02-17 12:15:27 +0100

    Add files via upload

--- views.py
+++ views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from dans_ma_rue.models import *
+from datetime import datetime
 
 def index(request):
-    test = ""
-    return render(request,'index.html', {'test': test})
+
+    return render(request,'index.html', {
+        "date": datetime.today()}
+        )
