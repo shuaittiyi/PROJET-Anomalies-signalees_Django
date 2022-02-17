fc8ade1d55d6d2f801c059549126067f915ccb9f
shuaittiyi <95615474+shuaittiyi@users.noreply.github.com>
2022-02-17 13:47:36 +0100

    Delete dans_ma_rue directory

--- dans_ma_rue/arrondissement/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    test = "Test pour voir"
-    return render(request, 'arrondissement/index.html', { 'test': test})
-
